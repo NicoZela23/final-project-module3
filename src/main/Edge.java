b6a9b2bd5f2ca0fde76b91cac87b0468acbb2825
NicoZela23 <fezelayani@gmail.com>
2024-02-15 11:06:22 -0400

    Edge, Nodes and gitignore added

---
+++ src/main/Edge.java
@@ -0,0 +1,28 @@
+package main;
+
+public class Edge implements Comparable<Edge>{
+
+    Node source;
+    Node destination;
+    double weight;
+
+    public Edge(Node source, Node destination, double weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public String toString(){
+        return String.format("%s--> %s, %f", source.name, destination.name, weight);
+    }
+
+    @Override
+    public int compareTo(Edge E) {
+        if (this.weight > E.weight){
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+}
---
+++ src/main/Node.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.LinkedList;
+
+public class Node {
+    public int id;
+    public String name;
+    private boolean visited;
+    LinkedList<Edge> edges;
+
+    Node(int id, String nameOfLocation){
+        this.id = id;
+        this.name = nameOfLocation;
+        visited = false;
+        edges = new LinkedList<>();
+    }
+    boolean isVisited(){
+        return visited;
+    }
+
+    void visit(){
+        visited = true;
+    }
+    void unvisit(){
+        visited = false;
+    }
+}
